f6463b44ca47d83435a3b1514fe9fb2734cf61f9
skymer156 <46322826+skymer156@users.noreply.github.com>
2022-03-05 11:40:15 +0900

    update vue app

---
+++ docs/js/about.49be90cd.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkjob_hunting_tool"]=self["webpackChunkjob_hunting_tool"]||[]).push([[443],{7381:function(n,t,u){u.r(t),u.d(t,{default:function(){return h}});var o=u(3396);const a={class:"about"},e=(0,o._)("h1",null,"This is an about page",-1),r=[e];function s(n,t){return(0,o.wg)(),(0,o.iD)("div",a,r)}var i=u(89);const c={},l=(0,i.Z)(c,[["render",s]]);var h=l}}]);
+//# sourceMappingURL=about.49be90cd.js.map
---
+++ src/views/EStools.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="EStools">
+    <h1>EStools</h1>
+    <p>ES Contains Counter</p>
+    <textarea name="textarea" id="contains" cols="80" rows="10" v-model="text"></textarea>
+    <div>{{text.length}}文字</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'EsTools',
+  data() {
+    return {
+      text: ''
+    }
+  },
+}
+</script>
