791b072101bc3b05a630580485f1f759cef05147
skymer156 <46322826+skymer156@users.noreply.github.com>
2022-03-05 12:19:48 +0900

    update vue config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === 'prodction'
-    ? '/docs'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/skymer_vue_job_hunting_tool/'
     : '/'
 })
