8fba0dfac9ec1b3fa9a35d95170455f1c493120a
skymer156 <46322826+skymer156@users.noreply.github.com>
2022-03-05 12:04:49 +0900

    update

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === 'prodction'
+    ? '/docs'
+    : '/'
 })
